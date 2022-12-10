ce029cabf6c9423459739531fbb1abd596e98b6f
Guilherme Emerick <gulhermeemerick@gmail.com>
2022-12-10 16:01:37 +0800

    new project

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import "./App.css";
+import "./index.css";
+
+function App() {
+  return (
+    <>
+      <div className="landing-page">
+        <div className="nav-menu">
+          <img src="" alt="logo" />
+          <div className="menu-items">
+            <a href="#menu">About</a>
+            <a href="#experience">Experience</a>
+            <a href="#work">Work</a>
+            <a href="#contact">Contact</a>
+          </div>
+        </div>
+
+        <div className="content">
+          <span>
+            <p>Hi my name is</p>
+            <p>Guilherme Emerick</p>
+            <p>I build things for the Web</p>
+            <p>
+              I've always been passionate about technology and nowadays my
+              hobbie and profession is to give life to websites. Welcome to my
+              personal website and feel free to get in touch.
+            </p>
+          </span>
+
+          <button type="button">Check out My Projects</button>
+        </div>
+          <div className="back-image">
+          </div>
+          <div className="back-image2"></div>
+      </div>
+    </>
+  );
+}
+
+export default App;
